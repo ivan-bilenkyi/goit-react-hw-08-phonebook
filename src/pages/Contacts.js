530d367a25d98cf3e6e8be84b9b6a83b61750f6a
Ivan Bilenkyi <ivanbilenkyi@gmail.com>
2023-12-17 00:24:56 +0200

    fix bag

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,13 +1,11 @@
 import { ContactList } from 'components/Contacts/ContactList';
 import { PhonebookForm } from 'components/PhonebookForm/PhonebookForm';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectIsLoading } from '../redux/contacts/selectors';
+import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchContacts } from '../redux/contacts/operations';
 
 export default function Contacts() {
   const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
 
   useEffect(() => {
     dispatch(fetchContacts());
